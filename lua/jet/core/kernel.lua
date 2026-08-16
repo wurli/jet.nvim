10dbc307c0d7483d27904b38566ec493dfdd5ba1
Jacob Scott <jscott2718@gmail.com>
2026-08-16 20:37:47 +0100

    refactor(kernel): pull update_execution_state() into dedicated method of Kernel

--- lua/jet/core/kernel.lua
+++ lua/jet/core/kernel.lua
@@ -404,64 +404,65 @@ end
 ---@return boolean
 function Kernel:has_lua_client() return self.client_id ~= nil end
 
+local last_execute_id = ""
+
 ---@private
-function Kernel:handle_stream()
-	local last_execute_id = ""
+---@param msg jet.jupyter.msg
+function Kernel:update_execution_state(msg)
+	local header = msg.header
+	local parent = msg.parent_header or {}
 
-	---@param msg jet.jupyter.msg
-	local update_execution_state = function(msg)
-		local header = msg.header
-		local parent = msg.parent_header or {}
-
-		-- Execution is only officially started once we receive "busy" status,
-		-- so for now just save the executed code so we can include it when do
-		-- get the "busy" status.
-		if header.msg_type == "execute_input" and parent.msg_id == last_execute_id and self.last_execution then
-			self.last_execution.code = msg.content.code
-			self.last_execution.count = msg.content.execution_count
-			return
-		end
-
-		if header.msg_type == "error" and parent.msg_id == last_execute_id and self.last_execution then
-			self.last_execution.is_error = true
-			return
-		end
-
-		if header.msg_type ~= "status" then
-			return
-		end
-		local new_state = msg.content and msg.content.execution_state
-		if not vim.tbl_contains({ "idle", "busy", "starting" }, new_state) then
-			utils.log_warn("Kernel '%s' sent unknown execution state: %s", self.spec.display_name, new_state)
-			return
-		end
-
-		self.execution_state = new_state
-
-		if new_state == "busy" and parent.msg_type == "execute_request" then
-			last_execute_id = parent.msg_id or last_execute_id
-			local count = self.last_execution and self.last_execution.count or 0
-			self.last_execution = { start_time = os.time(), count = count + 1 }
-		elseif new_state == "idle" and parent.msg_id == last_execute_id and self.last_execution then
-			self.last_execution.end_time = os.time()
-		end
-
-		if self.term and self.term.buf and vim.api.nvim_buf_is_valid(self.term.buf) then
-			local jet_b = vim.b[self.term.buf].jet or {}
-			jet_b.execution_state = self.execution_state
-			jet_b.last_execution = self.last_execution
-			vim.b[self.term.buf].jet = jet_b
-			vim.api.nvim__redraw({ statusline = true, buf = self.term.buf })
-		end
-
-		hooks.execution_state_changed(self, new_state)
+	-- Execution is only officially started once we receive "busy" status,
+	-- so for now just save the executed code so we can include it when do
+	-- get the "busy" status.
+	if header.msg_type == "execute_input" and parent.msg_id == last_execute_id and self.last_execution then
+		self.last_execution.code = msg.content.code
+		self.last_execution.count = msg.content.execution_count
+		return
 	end
 
+	if header.msg_type == "error" and parent.msg_id == last_execute_id and self.last_execution then
+		self.last_execution.is_error = true
+		return
+	end
+
+	if header.msg_type ~= "status" then
+		return
+	end
+	local new_state = msg.content and msg.content.execution_state
+	if not vim.tbl_contains({ "idle", "busy", "starting" }, new_state) then
+		utils.log_warn("Kernel '%s' sent unknown execution state: %s", self.spec.display_name, new_state)
+		return
+	end
+
+	self.execution_state = new_state
+
+	if new_state == "busy" and parent.msg_type == "execute_request" then
+		last_execute_id = parent.msg_id or last_execute_id
+		local count = self.last_execution and self.last_execution.count or 0
+		self.last_execution = { start_time = os.time(), count = count + 1 }
+	elseif new_state == "idle" and parent.msg_id == last_execute_id and self.last_execution then
+		self.last_execution.end_time = os.time()
+	end
+
+	if self.term and self.term.buf and vim.api.nvim_buf_is_valid(self.term.buf) then
+		local jet_b = vim.b[self.term.buf].jet or {}
+		jet_b.execution_state = self.execution_state
+		jet_b.last_execution = self.last_execution
+		vim.b[self.term.buf].jet = jet_b
+		vim.api.nvim__redraw({ statusline = true, buf = self.term.buf })
+	end
+
+	hooks.execution_state_changed(self, new_state)
+end
+
+---@private
+function Kernel:handle_stream()
 	utils.poll(self.stream, function(res)
 		if not res then
 			return "exit"
 		elseif res.status == "busy" then
-			update_execution_state(res.msg)
+			self:update_execution_state(res.msg)
 			self:update_output_stream(res.msg)
 			hooks.message_received(self, res.msg)
 			for _, hook in pairs(self.on_message_received) do
