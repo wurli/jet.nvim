8b20d4fdc4cf626c318716e47f5401208d1f5f82
Jacob Scott <jscott2718@gmail.com>
2026-08-24 23:55:31 +0100

    test(lsp): add a test for lsp completions

--- lua/jet/core/kernel/lsp.lua
+++ lua/jet/core/kernel/lsp.lua
@@ -16,7 +16,6 @@ Lsp.__index = Lsp ---@private
 ---@param opts jet.Lsp.init.Opts
 function Lsp.init(opts)
 	local out = setmetatable({
-		sesssion = opts.session_id,
 		name = string.format(
 			"jet_%s_%s",
 			opts.display_name:gsub("%W", "_"):gsub("_+", "_"):gsub("^_+", ""):gsub("_+$", ""),
@@ -29,41 +28,33 @@ function Lsp.init(opts)
 	return out
 end
 
-local make_capabilities = function()
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	return {
-		general = capabilities.general,
-		textDocument = {
-			completion = (capabilities.textDocument or {}).completion,
-			-- hover = {
-			-- 	dynamicRegistration = true,
-			-- 	contentFormat = { constants.MarkupKind.Markdown, constants.MarkupKind.PlainText },
-			-- },
-		},
-	}
-end
-
 function Lsp:configure()
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
 	vim.lsp.config(self.name, {
 		cmd = vim.lsp.rpc.connect("127.0.0.1", self.port),
 		root_markers = { ".git" },
 		filetypes = { self.filetype },
 		root_dir = ".",
-		capabilities = make_capabilities(),
+		capabilities = {
+			general = capabilities.general,
+			textDocument = {
+				completion = (capabilities.textDocument or {}).completion,
+				-- hover = {
+				-- 	dynamicRegistration = true,
+				-- 	contentFormat = { constants.MarkupKind.Markdown, constants.MarkupKind.PlainText },
+				-- },
+			},
+		},
 	})
 end
 
 function Lsp:enable()
 	if not vim.lsp.is_enabled(self.name) then
-		print("enabling " .. vim.inspect(self))
 		self:configure()
 		vim.lsp.enable(self.name)
 	end
 end
 
-function Lsp:disable()
-	print("disabling " .. vim.inspect(self))
-	vim.lsp.enable(self.name, false)
-end
+function Lsp:disable() vim.lsp.enable(self.name, false) end
 
 return Lsp
