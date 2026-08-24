5811428a1ce4974f51de5cb72ff7ed3badb43451
Jacob Scott <jscott2718@gmail.com>
2026-08-24 10:39:00 +0100

    test(img): add checks for buffer creation and display

--- tests/test_image.lua
+++ tests/test_image.lua
@@ -9,12 +9,13 @@ local T = new_set({
 	},
 })
 
-T["Images are produced by jet.nvim"] = function()
+T["Images work"] = function()
 	child.lua([[
 		_G.k = require("jet.core.kernel").init_owned({ spec_path = "test-kernels/ark/kernel.json" })
 
-		require("jet.core.config").options.hooks.on_image_display_pre.test = function(_, src)
+		require("jet.core.config").options.hooks.on_image_display_pre.test = function(k, src)
 			_G.rendered_image = src
+			_G.image_buf = k.img.buf
 		end
 
 		_G.k:start_lua_client(function(k)
@@ -23,11 +24,29 @@ T["Images are produced by jet.nvim"] = function()
 		end)
 	]])
 
+	-- file is created
 	vim.wait(5000, function() return child.lua_get("_G.rendered_image") ~= vim.NIL end)
 	local file = child.lua_get("_G.rendered_image")
 	file = file ~= vim.NIL and file or nil
 	assert(file, "No image was produced by the kernel")
 	assert(vim.uv.fs_stat(tostring(file)), string.format("Reported file %s does not exist", file))
+
+	-- Buffer is created
+	local buf_exists = vim.wait(5000, function()
+		local buf_session_id = child.lua_get("vim.b[_G.image_buf].jet.session_id")
+		return buf_session_id ~= vim.NIL
+	end)
+	assert(buf_exists, "Image buffer was not created")
+
+	-- Buffer is displayed
+	local buf_visible = vim.wait(5000, function()
+		for _, win in ipairs(child.api.nvim_list_wins()) do
+			if child.api.nvim_win_get_buf(win) == child.lua_get("_G.image_buf") then
+				return true
+			end
+		end
+	end)
+	assert(buf_visible, "Image buffer is not visible in any window")
 end
 
 return T
